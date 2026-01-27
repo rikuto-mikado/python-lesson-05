e7f558bd879126e2eb866349688ab29974952854
Rikuto <mikadorikuto@gmail.com>
2026-01-27 23:18:16 +0900

    update the code

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -8,8 +8,9 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    # **kwargs captures additional keyword arguments from the parent class
     def get_context_data(self, **kwargs):
-        context = {}
+        context = super().get_context_data(**kwargs)
         context["meals"] = MEAL_TYPE
         return context
 
